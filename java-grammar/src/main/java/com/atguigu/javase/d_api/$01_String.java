83cb9e24a03113dd20c5b0803d4f4fa9c16a8722
2368434946 <jcsune@163.com>
2023-09-02 23:25:20 +0800

    23.09.01 updated

---
+++ java-grammar/src/main/java/com/atguigu/javase/d_api/$01_String.java
@@ -0,0 +1,14 @@
+package com.atguigu.javase.d_api;
+
+/**
+ * String:字符串
+ *  ①所有带""都是String类的一个对象
+ */
+
+public class $01_String {
+    public static void main(String[] args) {
+        String s = "hello";
+        s="world";
+        System.out.println(s);
+    }
+}

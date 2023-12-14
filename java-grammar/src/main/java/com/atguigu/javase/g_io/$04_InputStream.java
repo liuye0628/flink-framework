0d1082f61ccf4c3d4069859281508531b6e685ef
2368434946 <jcsune@163.com>
2023-12-14 22:40:27 +0800

    23.12.14 updated

---
+++ java-grammar/src/main/java/com/atguigu/javase/g_io/$04_InputStream.java
@@ -0,0 +1,12 @@
+package com.atguigu.javase.g_io;
+
+/**
+ * 字节流:字节输入流
+ * java.io.InputStream抽象类是表示字节输入流所有类的超类,可以读取字节信息到内存中,它定义了字节输入流的基本共性功能方法
+ * · public void close(): 关闭此输入流并释放与此流相关联的任何系统资源
+ * · public abstract int read(): 从输入流读取数据的下一个字节
+ * · public int read(byte[] b): 从输入流中选取一些字节数,并将它们存储到字节数组b中
+ * 注意:close方法,当完成流的操作时,必须调用此方法,释放系统资源
+ */
+public class $04_InputStream {
+}
